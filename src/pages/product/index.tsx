e9af1e90549cd76c962969c3b15c517b6fb650ae
“Akbarwp” <akbarwp002@gmail.com>
2023-12-05 00:00:57 +0700

    NextJS-Dasar: Page & Layout

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -2,7 +2,7 @@ export default function Index() {
     return (
         <>
             <div className="w-full h-screen bg-white text-tradewind flex justify-center items-center">
-                <h1 className="text-4xl">Hello World</h1>
+                <h1 className="text-4xl">Product Page</h1>
             </div>
         </>
     );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+            colors: {
+                'fun-green': '#186049',
+                'eden': '#247158',
+                'tradewind': '#6AB29B',
+                'dawn-pink': '#E4D5C7',
+                'almond-forest': '#95877A'
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [
+        require("daisyui")
+    ],
 }
 export default config
